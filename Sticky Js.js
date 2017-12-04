d86b9eb436b3076fb0a8962bb2abb11bf19ac823
mahanandaray89 <mahanandaray9889@gmail.com>
2017-12-04 22:41:18 +0600

    Update Sticky Js.js

--- Sticky Js.js
+++ Sticky Js.js
@@ -1,3 +1,4 @@
+	===== I Uses This Code ====
 	var s = $(".header-sticky");
 	var pos = s.position();
 	    $(window).scroll(function () {
@@ -8,3 +9,30 @@
 	            s.removeClass("stick");
 	        }
 	 });
+
+	===== Another Sticky Code ====
+	var sticky_menu = $('#sticker');
+	    var pos = sticky_menu.position();
+	    if (sticky_menu.length) {
+		var windowpos = sticky_menu.offset().top;
+		$(window).on('scroll', function() {
+		    var windowpos = $(window).scrollTop();
+		    if (windowpos > pos.top) {
+			sticky_menu.addClass('stick');
+		    } else {
+			sticky_menu.removeClass('stick');
+		    }
+		});
+	    }
+
+	===== Another Sticky Code ====
+	$(window).scroll(function() {
+	if ($(this).scrollTop() > 32){  
+	    $('.header').addClass("sticky");
+	  }
+	  else{
+	    $('.header').removeClass("sticky");
+	  }
+	});
+
+ 
